11b40aab125e57c873d69f9ae02ecc9d8468b58b
ifycode <marychord@gmail.com>
2021-02-21 14:56:29 -0800

    Use try-catch block instead inside app.listen in srcServer.js

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,10 +4,10 @@ import open from 'open';
 const port = 3000;
 const app = express();
 
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
+app.listen(port, () => {
+  try {
     open(`http://localhost:${port}`);
+  }catch(err) {
+    console.log(err);
   }
 });
