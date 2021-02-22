d455079f82adf6724cc7e616ccb1c74358326d15
ifycode <marychord@gmail.com>
2021-02-22 09:37:12 -0800

    Handling errors for incorrect routes

--- buildScripts/app.js
+++ buildScripts/app.js
@@ -14,9 +14,26 @@ app.use(bodyParser.json());
 
 app.use('/users', expressRouter);
 
-app.use(( req, res, next) => {
+/*
+app.use((req, res, next) => {
   res.status(200).json({
     message: 'App works!'
+  });
+});
+*/
+
+app.use((req, res, next) => {
+  const error = new Error('Route not found!');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
   });
 });
 
