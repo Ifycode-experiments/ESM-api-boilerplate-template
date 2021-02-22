121f0ee1958db115016b66d08e6116c290fe89ff
ifycode <marychord@gmail.com>
2021-02-22 08:13:07 -0800

    Install & add morgan to app.js

--- buildScripts/app.js
+++ buildScripts/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 import mongooseModuleExport from './db'; //eslint-disable-line no-unused-vars
 import { router as expressRouter } from './api/controllers/userController';
 
 const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 const users = '/users';
