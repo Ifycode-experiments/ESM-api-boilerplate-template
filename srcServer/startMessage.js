fdc8febaa1cad77eabcaadb16c0320f90a457bae
ifycode <marychord@gmail.com>
2021-03-11 08:12:04 -0800

    Update startMessage.js to rhyme with readme

--- srcServer/startMessage.js
+++ srcServer/startMessage.js
@@ -1,4 +1,4 @@
 import chalk from 'chalk';
 
-let appName = 'insert-your-app-name';
-console.log( chalk.magenta(`\n${appName} running in dev mode\n`) ); //eslint-disable-line no-console
+let projectName = 'insert-your-project-name';
+console.log( chalk.magenta(`\n${projectName} running in dev mode\n`) ); //eslint-disable-line no-console
