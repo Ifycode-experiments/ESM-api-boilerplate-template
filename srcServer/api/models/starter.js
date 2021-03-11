0cf786edf65607bdf1a011b78f2445e8aceb2da5
ifycode <marychord@gmail.com>
2021-03-11 01:07:31 -0800

    Change Starter schema properties

--- srcServer/api/models/starter.js
+++ srcServer/api/models/starter.js
@@ -2,9 +2,7 @@ import mongoose from 'mongoose';
 
 const StarterSchema = mongoose.Schema({
   name: { type: String, required: true },
-  address: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: Number, required: true }
+  age: { type: Number, required: true }
 });
 
 let StarterModel = mongoose.model('Starter', StarterSchema);
