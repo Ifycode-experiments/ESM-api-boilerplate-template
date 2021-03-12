e4769d5e507e88f9cbcd60db0a9c41812a8932bd
ifycode <marychord@gmail.com>
2021-03-12 09:10:45 -0800

    fix grammar in mongodb atlas

--- srcServer/atlas/db.js
+++ srcServer/atlas/db.js
@@ -18,11 +18,11 @@ err => {
     console.log( chalk.redBright(`\nError in ATLAS DB connection: ${err.message} \n`) );
     console.log( chalk.yellowBright(`To use MongoDB ATLAS option, make sure you:
     => Have wifi/ data turned on and are connected to mongoDB atlas
-       - See https://docs.atlas.mongodb.com/getting-started/.
+       - See https://docs.atlas.mongodb.com/getting-started/
     => Update srcServer > atlas > db.js with your username, password, clusterName and DBname respectively
        - ensure you didn't mispell or include wrong details.
 
-  To use or see guide for installed mongoDB:
+  To use or see guide for installed mongoDB option:
     => Comment out import for === MongoDB ATLAS db === in srcServer > app.js,
       and use import for === Installed mongoDB's db === instead.
     `
