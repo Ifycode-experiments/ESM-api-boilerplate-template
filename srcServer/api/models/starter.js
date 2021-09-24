1f906b1abdc892a1a62dce3917f3983e07ce621b
Ifycode <marychord@gmail.com>
2021-09-24 06:16:13 +0100

    Fix mongoose pluralization by repeating collection name

--- srcServer/api/models/starter.js
+++ srcServer/api/models/starter.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 
+let collectionName = 'quick-tour';
+
 const StarterSchema = mongoose.Schema({
   name: { type: String, required: true },
   age: { type: Number, required: true }
 });
 
-let StarterModel = mongoose.model('Starter', StarterSchema);
+let StarterModel = mongoose.model(collectionName, StarterSchema, collectionName); //declare 'Starter' a second time to prevent mongoose from pluralizing or adding 's' to this collection name
 
 export { StarterModel };
