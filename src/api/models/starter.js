4352742aa7fc505390cfa17bb81bf07cd7527cd1
Ifycode <marychord@gmail.com>
2021-09-24 09:23:48 +0100

    Fix starter model collection name comment

--- src/api/models/starter.js
+++ src/api/models/starter.js
@@ -7,6 +7,6 @@ const StarterSchema = mongoose.Schema({
   age: { type: Number, required: true }
 });
 
-let StarterModel = mongoose.model(collectionName, StarterSchema, collectionName); //declare 'Starter' a second time to prevent mongoose from pluralizing or adding 's' to this collection name
+let StarterModel = mongoose.model(collectionName, StarterSchema, collectionName); //declare collection name a second time to prevent mongoose from pluralizing or adding 's' to the collection name
 
 export { StarterModel };
